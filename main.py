34a1ff5ebb16dcbac8025f65079eee07740b6dbc
rjknisk <rajdhamani96@gmail.com>
2025-07-24 14:16:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("Welcome to Prorj Toolbox!")
-print("More tools coming soon...")
+def main():
+    print("Welcome to Prorj - your smart toolbox!")
+    # Future: Add features here!
+
+if __name__ == "__main__":
+    main()
